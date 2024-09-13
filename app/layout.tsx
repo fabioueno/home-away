76917d72955e3a2ceb15a77dc98f6823c0e3938c
Fábio Ueno <fabio.t.ueno@gmail.com>
2024-09-13 12:34:56 +0200

    Create Navbar component's structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,23 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "HomeAway",
   description: "Feel at home, away from home",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <Navbar />
+        <main className="container py-10">
+          {children}
+        </main>
       </body>
     </html>
   );
