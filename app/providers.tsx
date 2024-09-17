056d9711b686de46517970e735ffcc944040c21e
Fábio Ueno <fabio.t.ueno@gmail.com>
2024-09-17 21:02:49 +0200

    Implement SignOutLink component

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,10 +2,12 @@
 
 import React from "react";
 import {ThemeProvider} from "@/app/theme-providers";
+import { Toaster } from "@/components/ui/toaster";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Toaster />
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         {children}
       </ThemeProvider>
--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,6 +1,21 @@
+"use client";
+
+import {useToast} from "@/hooks/use-toast";
+import {SignOutButton} from "@clerk/nextjs";
+
 function SignOutLink() {
+  const {toast} = useToast();
+  const handleSignOut = () => {
+    toast({
+      description: "You have been signed out"
+    });
+  }
   return (
-    <div>SignOutLink</div>
+    <SignOutButton redirectUrl="/">
+      <button className="w-full text-left" onClick={handleSignOut}>
+        Sign out
+      </button>
+    </SignOutButton>
   );
 }
 
